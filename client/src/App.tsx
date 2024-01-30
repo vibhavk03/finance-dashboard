3f21b0f6e615118ab0bf1b400f0c7b5af802a78b
vibhavk03 <vibhavkashyap3@gmail.com>
2024-01-30 21:45:43 +0530

    adding mui theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
+import { themeSettings } from './theme';
+import { CssBaseline } from '@mui/material';
+
 function App() {
-  return <div className='app'></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        hi
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
