fea39d6b242c56f2ea002789bddee3fccaafb765
vibhavk03 <vibhavkashyap3@gmail.com>
2024-02-04 11:42:29 +0530

    adding routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,24 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { useMemo } from 'react';
 import { themeSettings } from './theme';
-import { CssBaseline } from '@mui/material';
+import { Box, CssBaseline } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        hi
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Routes>
+              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/predictions" element={<div>predictions</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
