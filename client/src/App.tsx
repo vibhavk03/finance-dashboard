d8bc4a4e615905a587651a0d76413c4de14be535
vibhavk03 <vibhavkashyap3@gmail.com>
2024-02-04 15:20:45 +0530

    adding navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { useMemo } from 'react';
 import { themeSettings } from './theme';
 import { Box, CssBaseline } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from '@/scenes/navbar';
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -12,6 +13,7 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
             <Routes>
               <Route path="/" element={<div>dashboard page</div>} />
               <Route path="/predictions" element={<div>predictions</div>} />
