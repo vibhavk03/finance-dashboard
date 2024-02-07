7c2eb6ca1a72ebd669792942d1d7eb2983a8db0b
vibhavk03 <vibhavkashyap3@gmail.com>
2024-02-07 21:28:44 +0530

    adding get kpi route

--- server/index.js
+++ server/index.js
@@ -1,21 +1,26 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import helmet from "helmet";
-import morgan from "morgan";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import kpiRoutes from './routes/kpi.js';
 
 /* configurations */
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+/* routes */
+app.use('/kpi', kpiRoutes);
 
 /* mongodb setup */
 const PORT = process.env.PORT || 9000;
