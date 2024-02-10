749efa8d3c19b5b670e70970271eef94eebef9c5
vibhavk03 <vibhavkashyap3@gmail.com>
2024-02-10 17:44:54 +0530

    adding product route

--- server/index.js
+++ server/index.js
@@ -7,8 +7,11 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 
 import kpiRoutes from './routes/kpi.js';
+import ProductRoutes from './routes/product.js';
+
 import KPI from './models/KPI.js';
-import { kpis } from './data/data.js';
+import Product from './models/Product.js';
+import { kpis, products } from './data/data.js';
 
 /* configurations */
 dotenv.config();
@@ -23,6 +26,7 @@ app.use(cors());
 
 /* routes */
 app.use('/kpi', kpiRoutes);
+app.use('/product', ProductRoutes);
 
 /* mongodb setup */
 const PORT = process.env.PORT || 9000;
@@ -34,5 +38,6 @@ mongoose
     /* adding data to db once */
     // await mongoose.connection.db.dropDatabase();
     // KPI.insertMany(kpis);
+    // Product.insertMany(products);
   })
   .catch((error) => console.log(`${error} did not connect`));
