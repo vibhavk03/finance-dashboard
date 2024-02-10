ad19d5b86e1b9e3a643f6e1d1eeba2b67061feba
vibhavk03 <vibhavkashyap3@gmail.com>
2024-02-10 19:33:09 +0530

    format file

--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
-import kpiRoutes from './routes/kpi.js';
-import ProductRoutes from './routes/product.js';
-import transactionRoutes from './routes/transaction.js';
-
 import KPI from './models/KPI.js';
 import Product from './models/Product.js';
 import Transaction from './models/Transaction.js';
 import { kpis, products, transactions } from './data/data.js';
+
+import kpiRoutes from './routes/kpi.js';
+import productRoutes from './routes/product.js';
+import transactionRoutes from './routes/transaction.js';
 
 /* configurations */
 dotenv.config();
@@ -28,7 +28,7 @@ app.use(cors());
 
 /* routes */
 app.use('/kpi', kpiRoutes);
-app.use('/product', ProductRoutes);
+app.use('/product', productRoutes);
 app.use('/transaction', transactionRoutes);
 
 /* mongodb setup */
