5b14ae6bb6f33cfdd3cf31ccbba12d6c4194b9be
vibhavk03 <vibhavkashyap3@gmail.com>
2024-03-12 13:54:00 +0530

    fix tslint warnings

--- client/src/scenes/dashboard/Row2.tsx
+++ client/src/scenes/dashboard/Row2.tsx
@@ -144,7 +144,7 @@ const Row2 = () => {
               paddingAngle={2}
               dataKey="value"
             >
-              {pieData.map((entry, index) => (
+              {pieData.map((_entry, index) => (
                 <Cell key={`cell-${index}`} fill={pieColors[index]} />
               ))}
             </Pie>
